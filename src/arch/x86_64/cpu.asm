f95e2d3b8833e722db1a4c3531a3fd0cf6f0a590
Frederic Meyer <binarynoob96@gmail.com>
2018-10-08 06:32:52 +0200

    [x86_64] Place GDT/IDT/FPU assembly in single file.

--- src/arch/x86_64/cpu.asm
+++ src/arch/x86_64/cpu.asm
@@ -7,11 +7,58 @@
 
 section .text
 
+global gdt_reload
+global idt_reload
+global _cpuid
 global fpu_init
 
 CR0_MONITOR_COPROC equ (1<< 1)
 CR4_OSFXSR         equ (1<< 9)
 CR4_OSXMMEXCPT     equ (1<<10)
+
+; Reload GDT register
+;   rdi(arg0) = GDT pointer
+;   rsi(arg1) = code segment
+;   rdx(arg2) = data segment
+gdt_reload:
+    lgdt [rdi]
+
+    ; Load segments
+    mov rax, rdx
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Far return
+    mov rax, .gdt_done
+    push rsi
+    push rax
+    db 0x48 ; REX-prefix
+    retf
+.gdt_done:
+    ret
+
+; Reload IDT table
+;   rdi(arg0) = IDT pointer
+idt_reload:
+    lidt [rdi]
+    ret
+
+; Get processor information
+;   rdi(arg0): cpuid function
+;   rsi(arg1): output structure
+_cpuid:
+    mov eax, edi
+    
+    cpuid
+    mov [rsi + 0x0], eax
+    mov [rsi + 0x4], ebx
+    mov [rsi + 0x8], ecx
+    mov [rsi + 0xC], edx
+    
+    ret
 
 fpu_init:
     ; Enable coprocessor monitoring.
