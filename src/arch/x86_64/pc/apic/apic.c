5bbc025ccfdf5b275fa36dafd17c0fa2a1e206de
Frederic Meyer <binarynoob96@gmail.com>
2018-10-24 00:12:20 +0200

    [x86_64][pc] apic: locate MP table.

--- src/arch/x86_64/pc/apic/apic.c
+++ src/arch/x86_64/pc/apic/apic.c
@@ -8,7 +8,9 @@
 
 #include "apic.h"
 #include <stdint.h>
+#include <stddef.h>
 #include <arch/print.h>
+#include <arch/x86_64/pc/vm/vm.h>
 
 #define MSR_APIC_BASE  (0x1B)
 #define MSR_APIC_BASE_ENABLE (0x800)
@@ -16,6 +18,8 @@
 
 void _rdmsr(uint32_t reg, uint32_t* eax, uint32_t* edx);
 void _wrmsr(uint32_t reg, uint32_t  eax, uint32_t  edx);
+
+static void* lapic_mmio = NULL;
 
 bool lapic_is_present(struct amd64_cpu* cpu) {
     return cpu->features & CPUID_FEAT_APIC;
@@ -36,16 +40,40 @@ static void lapic_set_base(uint64_t base) {
     _wrmsr(MSR_APIC_BASE, (base&0xFFFFFFFF), (base>>32));
 }
 
+static void find_mpc_table() {
+    void* data = (void*)0xFFFF808000000000;
+
+    /* TODO: atm we are scanning the entire first 1MiB.
+     * However the specification allows us to narrow it down
+     * to three specific memory areas.
+     */
+    for (int i = 0; i <= 0xFFFFC; i++) {
+        if (*(uint32_t*)(data + i) == 0x5F504D5F) { /* _MP_ */
+            info("lapic: Floating Pointer Structure @ %p", data + i);
+            return;
+        }        
+    }
+}
+
 void lapic_init() {
     uint64_t base = lapic_get_base();
 
-    /* enable the LAPIC */
+    /* Enable the Local-APIC */
     info("lapic: base address is 0x%016llX", base);
     lapic_set_base(base);
     info("lapic: enabled Local-APIC.");
 
-    /* start receiving interrupts 
-     * this is triggering a page fault right now. */
-    volatile uint32_t* spivr = (void*)base + 0xF0;
-    //*spivr |= 0x80;
+    /* Map its MMIO registers into memory */
+    lapic_mmio = vm_alloc(1);
+    vm_map_page(lapic_mmio, base / 4096);
+    info("lapic: mapped Local-APIC MMIO @ %p", lapic_mmio);
+
+    volatile uint32_t* spivr = (void*)lapic_mmio + 0xF0;
+
+    /* Actually enabling the Local-APIC by setting
+     * Enable-bit in the SPIVR register.
+     */    
+    *spivr |= 0x80;
+
+    find_mpc_table();
 }
