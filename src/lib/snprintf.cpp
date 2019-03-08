444984523c4769209cce2cc1462d9dcb73791135
Frederic Meyer <binarynoob96@gmail.com>
2019-03-08 23:39:40 +0100

    [lib] snprintf: Improved code styling and naming.

--- src/lib/snprintf.cpp
+++ src/lib/snprintf.cpp
@@ -27,12 +27,14 @@ struct FormatState {
     const char* format; /* format string */
 
     struct Parameters {
-        char pad_chr = ' ';
-        int  pad_len = 0;
+        char pad_char = ' ';
+        int  pad_length = 0;
+
         bool justify_left = false;
         bool show_plus = false;
         bool pound = false;
         int  precision = -1;
+        
         DataType data_type = DataType::None;
     } parameters;
 
@@ -63,23 +65,29 @@ static inline void parse_flags(FormatState& state) {
 
     while (1) {
         switch (format[state.i]) {
-            case '-':
-                params.justify_left = true;
-                break;
-            case '+':
-                params.show_plus = true;
-                break;
-            case ' ':
-                /* TODO */
-                break;
-            case '#':
-                params.pound = true;
-                break;
-            case '0':
-                params.pad_chr = '0';
-                break;
-            default:
-                return;
+        case '-': {
+            params.justify_left = true;
+            break;
+        }
+        case '+': {
+            params.show_plus = true;
+            break;
+        }
+        case ' ': {
+            /* TODO */
+            break;
+        }
+        case '#': {
+            params.pound = true;
+            break;
+        }
+        case '0': {
+            params.pad_char = '0';
+            break;
+        }
+        default: {
+            return;
+        }
         }
         state.i++;
     }
@@ -93,9 +101,9 @@ static inline bool parse_width(FormatState& state, va_list arg) {
         return false;
 
     if (is_numeric_char(format[state.i])) {
-        params.pad_len = parse_number(state);
+        params.pad_length = parse_number(state);
     } else if (format[state.i] == '*') {
-        params.pad_len = va_arg(arg, int);
+        params.pad_length = va_arg(arg, int);
         state.i++;
     }
 
@@ -130,51 +138,57 @@ static inline bool parse_length(FormatState& state) {
         return false;
     
     switch (format[state.i]) {
-        case 'h':
-            if (format[state.i + 1] == 'h') {
-                params.data_type = DataType::Char;
-                state.i += 2;
-            } else {
-                params.data_type = DataType::ShortInteger;   
-                state.i++;
-            }
-            break;
-        case 'l':
-            if (format[state.i + 1] == 'l') {
-                params.data_type = DataType::LongLongInteger;
-                state.i += 2;
-            } else {
-                params.data_type = DataType::LongInteger;
-                state.i++;
-            }
-            break;
-        case 'j':
-            params.data_type = DataType::IntMax;
+    case 'h': {
+        if (format[state.i + 1] == 'h') {
+            params.data_type = DataType::Char;
+            state.i += 2;
+        } else {
+            params.data_type = DataType::ShortInteger;   
             state.i++;
-            break;
-        case 'z':
-            params.data_type = DataType::Size;
+        }
+        break;
+    }
+    case 'l': {
+        if (format[state.i + 1] == 'l') {
+            params.data_type = DataType::LongLongInteger;
+            state.i += 2;
+        } else {
+            params.data_type = DataType::LongInteger;
             state.i++;
-            break;
-        case 't':
-            params.data_type = DataType::PointerDifference;
-            state.i++;
-            break;
-        case 'L':
-            params.data_type = DataType::LongDouble;
-            state.i++;
-            break;
+        }
+        break;
+    }
+    case 'j': {
+        params.data_type = DataType::IntMax;
+        state.i++;
+        break;
+    }
+    case 'z': {
+        params.data_type = DataType::Size;
+        state.i++;
+        break;
+    }
+    case 't': {
+        params.data_type = DataType::PointerDifference;
+        state.i++;
+        break;
+    }
+    case 'L': {
+        params.data_type = DataType::LongDouble;
+        state.i++;
+        break;
+    }
     }
     
     return true;
 }
 
 static int get_left_pad(FormatState& state) {
-    return state.parameters.justify_left ? 0 : state.parameters.pad_len;
+    return state.parameters.justify_left ? 0 : state.parameters.pad_length;
 }
 
 static int get_right_pad(FormatState& state) {
-    return state.parameters.justify_left ? state.parameters.pad_len : 0;
+    return state.parameters.justify_left ? state.parameters.pad_length : 0;
 }
 
 static inline void append(FormatState& state, char chr) {
@@ -204,28 +218,35 @@ static inline bool fmt_signed_int(FormatState& state, va_list arg) {
 
     /* Get number to print */
     switch (params.data_type) {
-        case DataType::None:
-        case DataType::Char:
-        case DataType::ShortInteger:
-            value = va_arg(arg, int);
-            break;
-        case DataType::LongLongInteger:
-            value = va_arg(arg, long long);
-            break;
-        case DataType::LongInteger:
-            value = va_arg(arg, long);
-            break;
-        case DataType::IntMax:
-            value = va_arg(arg, intmax_t);
-            break;
-        case DataType::Size: /* checkme */
-            value = va_arg(arg, size_t);
-            break;
-        case DataType::PointerDifference: /* checkme */
-            value = va_arg(arg, ptrdiff_t);
-            break;
-        default:
-            return false; 
+    case DataType::None:
+    case DataType::Char:
+    case DataType::ShortInteger: {
+        value = va_arg(arg, int);
+        break;
+    }
+    case DataType::LongLongInteger: {
+        value = va_arg(arg, long long);
+        break;
+    }
+    case DataType::LongInteger: {
+        value = va_arg(arg, long);
+        break;
+    }
+    case DataType::IntMax: {
+        value = va_arg(arg, intmax_t);
+        break;
+    }
+    case DataType::Size: {
+        value = va_arg(arg, size_t);
+        break;
+    }
+    case DataType::PointerDifference: {
+        value = va_arg(arg, ptrdiff_t);
+        break;
+    }
+    default: {
+        return false;
+    } 
     }
 
     intmax_t temp = value;
@@ -266,13 +287,13 @@ static inline bool fmt_signed_int(FormatState& state, va_list arg) {
 
     /* Pad string to width */
     for (int i = 0; i < digits_start; i++)
-        buffer[i] = params.pad_chr;
+        buffer[i] = params.pad_char;
 
     /* Place sign character */
     if (show_sign) {
         char sign = (value < 0) ? '-' : '+';
 
-        if (params.pad_chr == ' ')
+        if (params.pad_char == ' ')
             buffer[digits_start-1] = sign;
         else
             buffer[0] = sign;
@@ -294,31 +315,39 @@ static inline bool fmt_unsigned_int(FormatState& state,
 
     /* Get number to print */
     switch (params.data_type) {
-        case DataType::None:
-        case DataType::Char:
-        case DataType::ShortInteger:
-            value = va_arg(arg, unsigned int);
-            break;
-        case DataType::LongInteger:
-            value = va_arg(arg, unsigned long int);
-            break;
-        case DataType::LongLongInteger:
-            value = va_arg(arg, unsigned long long int);
-            break;
-        case DataType::IntMax:
-            value = va_arg(arg, uintmax_t);
-            break;
-        case DataType::Size:
-            value = va_arg(arg, size_t);
-            break;
-        case DataType::PointerDifference:
-            value = va_arg(arg, ptrdiff_t);
-            break;
-        case DataType::Pointer:
-            value = (uintmax_t)va_arg(arg, void*);
-            break;
-        default:
-            return false;
+    case DataType::None:
+    case DataType::Char:
+    case DataType::ShortInteger: {
+        value = va_arg(arg, unsigned int);
+        break;
+    }
+    case DataType::LongInteger: {
+        value = va_arg(arg, unsigned long int);
+        break;
+    }
+    case DataType::LongLongInteger: {
+        value = va_arg(arg, unsigned long long int);
+        break;
+    }
+    case DataType::IntMax: {
+        value = va_arg(arg, uintmax_t);
+        break;
+    }
+    case DataType::Size: {
+        value = va_arg(arg, size_t);
+        break;
+    }
+    case DataType::PointerDifference: {
+        value = va_arg(arg, ptrdiff_t);
+        break;
+    }
+    case DataType::Pointer: {
+        value = (uintmax_t)va_arg(arg, void*);
+        break;
+    }
+    default: {
+        return false;
+    }
     }
 
     int prefix_length = 0;
@@ -363,11 +392,11 @@ static inline bool fmt_unsigned_int(FormatState& state,
 
     /* Pad string to width */
     for (int i = 0; i < digits_start; i++)
-        buffer[i] = params.pad_chr;
+        buffer[i] = params.pad_char;
 
     /* Print number system prefix (e.g. base 16 0x) */
     if (params.pound) {
-        int start = (params.pad_chr == '0') ? 0 : (digits_start - prefix_length);
+        int start = (params.pad_char == '0') ? 0 : (digits_start - prefix_length);
         for (int i = 0; i < prefix_length; i++)
             buffer[start + i] = prefix[i];
     }
@@ -422,35 +451,43 @@ static inline bool do_format(FormatState& state, va_list arg) {
     auto& params = state.parameters;
 
     switch (state.format[state.i]) {
-        /* Signed decimal */
-        case 'd':
-        case 'i':
-            return fmt_signed_int(state, arg);
+    /* Signed decimal */
+    case 'd':
+    case 'i': {
+        return fmt_signed_int(state, arg);
+    }
+    /* Unsigned integer */
+    case 'u': {
+        return fmt_unsigned_int(state, 10, "0123456789", "", arg);
+    }
+    case 'o': {
+        return fmt_unsigned_int(state, 8, "01234567", "0", arg);
+    }
+    case 'x': {
+        return fmt_unsigned_int(state, 16, "0123456789abcdef", "0x", arg);
+    }
+    case 'X': {
+        return fmt_unsigned_int(state, 16, "0123456789ABCDEF", "0X", arg);
+    }
 
-        /* Unsigned integer */
-        case 'u':
-            return fmt_unsigned_int(state, 10, "0123456789", "", arg);
-        case 'o':
-            return fmt_unsigned_int(state, 8, "01234567", "0", arg);
-        case 'x':
-            return fmt_unsigned_int(state, 16, "0123456789abcdef", "0x", arg);
-        case 'X':
-            return fmt_unsigned_int(state, 16, "0123456789ABCDEF", "0X", arg);
+    /* String & Char */
+    case 'c': {
+        return fmt_char(state, arg);
+    }
+    case 's': {
+        return fmt_string(state, arg);
+    }
 
-        /* String & Char */
-        case 'c':
-            return fmt_char(state, arg);
-        case 's':
-            return fmt_string(state, arg);
-
-        /* Misc. */
-        case 'n':
-            *(va_arg(arg, int*)) = (int)state.total;
-            return true;
-        case 'p':
-            params.pound = true;
-            params.data_type = DataType::Pointer;
-            return fmt_unsigned_int(state, 16, "0123456789abcdef", "0x", arg);
+    /* Misc. */
+    case 'n': {
+        *(va_arg(arg, int*)) = (int)state.total;
+        return true;
+    }
+    case 'p': {
+        params.pound = true;
+        params.data_type = DataType::Pointer;
+        return fmt_unsigned_int(state, 16, "0123456789abcdef", "0x", arg);
+    }
     }
     
     return false;
@@ -490,8 +527,8 @@ int vsnprintf(char* buffer, size_t size, const char* format, va_list arg) {
 
             /* Apparently glibc ignores '0'-padding when precision is defined.
              * TODO: check if this is expected / standard behaviour. */
-            if (params.pad_chr == '0' && params.precision > 0)
-                params.pad_chr = ' ';
+            if (params.pad_char == '0' && params.precision > 0)
+                params.pad_char = ' ';
 
             /* Finally perform the formatting operation */
             if (!do_format(state, arg))
@@ -509,11 +546,10 @@ int vsnprintf(char* buffer, size_t size, const char* format, va_list arg) {
 }
 
 int snprintf(char* buffer, size_t size, const char* format, ...) {
-    int total;
     va_list arg;
-
+    
     va_start(arg, format);
-    total = vsnprintf(buffer, size, format, arg);
+    int total = vsnprintf(buffer, size, format, arg);
     va_end(arg);
     return total;
 }
