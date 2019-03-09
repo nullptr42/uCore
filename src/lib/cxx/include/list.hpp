713b497e8a9f7b2844d094a8e9c3774ba9878214
Frederic Meyer <binarynoob96@gmail.com>
2019-03-09 21:24:09 +0100

    [lib] cxx: Implemented a generic List type.

---
+++ src/lib/cxx/include/list.hpp
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <stdio.hpp>
+
+namespace cxx {
+
+template <typename T>
+class List {
+    struct ListNode {
+        ListNode* prev;
+        ListNode* next;
+        T value;
+
+        ListNode();
+        ListNode(const T& value, ListNode* prev = nullptr,
+                                 ListNode* next = nullptr)
+            : prev(prev)
+            , next(next)
+            , value(value)
+        { }
+    };
+
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+
+    struct Iterator {
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        using iterator = Iterator;
+
+        ListNode* position;
+
+        Iterator() : position(nullptr) { }
+        Iterator(ListNode* node) : position(node) { }
+
+        iterator operator++(int) {
+            return position->next;
+        }
+
+        iterator& operator++() {
+            position = position->next;
+            return *this;
+        }
+
+        reference operator*() const {
+            return position->value;
+        }
+
+        pointer operator->() const {
+            return &position->value;
+        }
+
+        iterator operator+(difference_type value) const {
+            auto node = position;
+            for (auto i = 0; i < value; i++) {
+                node = node->next;
+                if (node == nullptr) {
+                    break;
+                }
+            }
+            return node;
+        }
+
+        bool operator==(const iterator& rhs) const {
+            return position == rhs.position;
+        }
+
+        bool operator!=(const iterator& rhs) const {
+            return position != rhs.position;
+        }
+    };
+public:
+
+    List() { }
+   ~List() { }
+
+    void InsertBack(const T& value) {
+        if (head == nullptr) {
+            head = new ListNode(value);
+            tail = head;
+        } else {
+            auto node = new ListNode(value, tail);
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    void InsertFront(const T& value) {
+        if (head == nullptr) {
+            head = new ListNode(value);
+            tail = head;
+        } else {
+            head = new ListNode(value, nullptr, head);
+        }
+    }
+
+    Iterator begin() {
+        return Iterator(head);
+    }
+
+    Iterator end() {
+        return Iterator(nullptr);
+    }
+};
+
+}
--- src/platform/pc/main.cpp
+++ src/platform/pc/main.cpp
@@ -1,11 +1,18 @@
 #include <stdint.h>
 #include <stdio.hpp>
+#include <list.hpp>
 
 extern "C" void fpu_init();
 
 extern "C" void __cxa_pure_virtual() {
     while (1) { }
 }
+
+struct MyStruct {
+    int a;
+    int b;
+    MyStruct(int a, int b) : a(a), b(b) { }
+};
 
 extern "C" void kernel_main(void) {
     fpu_init();
@@ -14,6 +21,22 @@ extern "C" void kernel_main(void) {
     cxx::printf("\e[2;32mXXXX is another test! :)\e[0m\rThis\n");
     cxx::printf("a\tb\t\tc\td\n");
     cxx::printf("Oh dear computer, just what is the answer? 0x%08x\n", 42);
+
+    cxx::List<MyStruct> list;
+
+    list.InsertBack({1, 2});
+    list.InsertBack({3, 4});
+
+    for (auto& item : list) {
+        item.a++;
+        item.b--;
+    }
+
+    for (auto item : list) {
+        cxx::printf("a=%d b=%d\n", item.a, item.b);
+    }
+
+    cxx::printf("Done!\n");
 
     while (1) { }
 }
