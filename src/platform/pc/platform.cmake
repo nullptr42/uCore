8f334ad95bdf5859bdf0ce8c99f20cbe6c197394
Frederic Meyer <binarynoob96@gmail.com>
2019-03-09 01:17:20 +0100

    [cmake] Rename project to match OS name.

--- src/platform/pc/platform.cmake
+++ src/platform/pc/platform.cmake
@@ -1,6 +1,6 @@
-set(MSPHERE_LD_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/layout.ld)
-set(MSPHERE_LD_FLAGS "-z max-page-size=4096")
-set(MSPHERE_ARCH "x86_64")
+set(REINIX_LD_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/layout.ld)
+set(REINIX_LD_FLAGS "-z max-page-size=4096")
+set(REINIX_ARCH "x86_64")
 
 set(PLATFORM_SRC
     platform/pc/boot.asm
