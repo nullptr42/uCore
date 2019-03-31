8a79751ace8a617534fe411500fe30be58031a69
Frederic Meyer <binarynoob96@gmail.com>
2019-03-31 04:52:09 +0200

    Get rid of LIBRARY_SRC

--- src/lib/library.cmake
+++ src/lib/library.cmake
@@ -1,12 +1,14 @@
 
-set(LIBRARY_SRC
-    src/lib/terminal/emulator.cpp
-    src/lib/cxx/snprintf.cpp
-    src/lib/cxx/printf.cpp
-)
-
 include_directories(
     ${CMAKE_CURRENT_LIST_DIR}/terminal/include
     ${CMAKE_CURRENT_LIST_DIR}/cxx/include
     ${CMAKE_CURRENT_LIST_DIR}/multiboot/include
+)
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/cxx)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/terminal/)
+
+set(REINIX_LIBRARY_LIST
+    rcxx
+    terminal
 )
