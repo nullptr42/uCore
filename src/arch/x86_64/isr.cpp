0c801b91be54bf4f85fc470e093eaba4ae559083
Frederic Meyer <binarynoob96@gmail.com>
2019-03-31 03:45:41 +0200

    Added a license.

--- src/arch/x86_64/isr.cpp
+++ src/arch/x86_64/isr.cpp
@@ -1,8 +1,12 @@
+/*
+ * Copyright (C) 2019 Frederic Meyer (fleroviux). All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #include <arch/x86_64/cpu_state.hpp>
 
 using namespace arch::x86_64;
 
-extern "C" CpuState *handle_interrupt(CpuState *cpu) {
-  /* TODO */
-  return cpu;
-}
+extern "C" CpuState *handle_interrupt(CpuState *cpu) { return cpu; }
--- src/kernel/version.cpp
+++ src/kernel/version.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2019 Frederic Meyer (fleroviux). All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 
 #include <kernel/version.hpp>
 
