4f0ed4bf408fa8e641019a2adb2061c8d214da56
Frederic Meyer <binarynoob96@gmail.com>
2019-04-03 00:50:09 +0200

    Setup types.h kernel header.

---
+++ include/reinix/types.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2019 Frederic Meyer (fleroviux). All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _REINIX_TYPES_H_
+#define _REINIX_TYPES_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
+#ifdef __x86_64__
+typedef uint64_t paddr_t;
+typedef uint64_t vaddr_t;
+#else
+#error "Unknown CPU architecture, either not detected or unsupported by reinix."
+#endif
+
+#endif // _REINIX_TYPES_H_
