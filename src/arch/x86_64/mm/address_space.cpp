773a7e073dc2b2f6f8c479416509d4e126871445
Frederic Meyer <fleroviux@gmail.com>
2019-05-25 14:10:15 +0200

    [x86_64] mm: Optimized block Map(...) operation.

--- src/arch/x86_64/mm/address_space.cpp
+++ src/arch/x86_64/mm/address_space.cpp
@@ -8,6 +8,7 @@
 #include "address_space.hpp"
 
 #include <kernel/memory/memory.hpp>
+#include <lib/rxx/stdio.hpp>
 
 using namespace arch::x86_64;
 
@@ -27,6 +28,7 @@ ptentry_t *X64_AddressSpace::GetOrCreateTable(ptentry_t *parent, int child) {
     if (kernel::g_frame_alloc->Alloc(hack, 0) !=
         kernel::FrameAllocator::Status::Success) {
       /* TODO: we ran out of memory, maybe we should panic.. */
+      rxx::printf("Ran out of memory!\n");
       while (1)
         ;
     }
@@ -55,6 +57,8 @@ int X64_AddressSpace::GetX64Flags(int flags) {
   return x64_flags;
 }
 
+/* TODO: decide how we handle virt/phys addresses that aren't on page boundaries. */
+
 void X64_AddressSpace::Map(vaddr_t virt, paddr_t phys, int flags) {
   int lvl4_idx = ((uint64_t)virt >> 12) & 0x1FF;
   int lvl3_idx = ((uint64_t)virt >> 21) & 0x1FF;
@@ -73,8 +77,6 @@ void X64_AddressSpace::Map(vaddr_t virt, paddr_t phys, int flags) {
 }
 
 void X64_AddressSpace::Map(vaddr_t virt, paddr_t phys, size_t size, int flags) {
-  /* TODO: implement this properly. */
-
   auto count = size / 4096;
 
   if ((size % 4096) != 0)
@@ -82,9 +84,52 @@ void X64_AddressSpace::Map(vaddr_t virt, paddr_t phys, size_t size, int flags) {
 
   phys &= ~0xFFF;
 
-  for (size_t i = 0; i < count; i++) {
-    Map(virt, phys, flags);
-    virt += 4096;
-    phys += 4096;
+  auto virt_s = virt;
+  auto virt_e = virt + (count - 1) * 4096;
+  auto pml4 = (ptentry_t *)BuildAddress(256, 256, 256, 256);
+
+  int s1 = (virt_s >> 39) & 0x1FF;
+  int e1 = (virt_e >> 39) & 0x1FF;
+
+  for (int i = s1; i <= e1; i++) {
+    int s2 = 0;
+    int e2 = 512;
+
+    if (i == s1)
+      s2 = (virt_s >> 30) & 0x1FF;
+    if (i == e1)
+      e2 = (virt_e >> 30) & 0x1FF;
+
+    auto pdpt = GetOrCreateTable(pml4, i);
+
+    for (int j = s2; j <= e2; j++) {
+      int s3 = 0;
+      int e3 = 512;
+
+      if (i == s1 && j == s2)
+        s3 = (virt_s >> 21) & 0x1FF;
+      if (i == e1 && j == e2)
+        e3 = (virt_e >> 21) & 0x1FF;
+
+      auto pd = GetOrCreateTable(pdpt, j);
+
+      for (int k = s3; k <= e3; k++) {
+        int s4 = 0;
+        int e4 = 512;
+
+        if (i == s1 && j == s2 && k == s3)
+          s4 = (virt_s >> 12) & 0x1FF;
+        if (i == e1 && j == e2 && k == e3)
+          e4 = (virt_e >> 12) & 0x1FF;
+
+        auto pt = GetOrCreateTable(pd, k);
+
+        for (int m = s4; m <= e4; m++) {
+          /* FIXME: use flags for GetX64Flags. */
+          pt[m] = GetX64Flags(PAGE_WRITABLE | PAGE_EXECUTE) | ptentry_t(phys);
+          phys += 4096;
+        }
+      }
+    }
   }
 }
